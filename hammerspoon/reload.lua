1148dd2928d49f0a5e259576e53625ba4ea27507
insv <insv23@outlook.com>
2024-06-17 20:25:00 +0800

    refact: reload.lua

--- hammerspoon/reload.lua
+++ hammerspoon/reload.lua
@@ -1,13 +1,19 @@
 -- Automatically reload the hammerspoon configuration file when changes are detected.
 -- 检测到 hammerspoon 配置文件发生变动就自动重新加载配置文件
-function reload(files)
-    for _, file in pairs(files) do
-      if file:sub(-4) == ".lua" then
-        hs.notify.new({title='Reloading', informativeText='Reloading Hammerspoon config'}):send()
-        hs.reload()
-        return
-      end
+
+function reloadConfig(files)
+    doReload = false
+    for _,file in pairs(files) do
+        if file:sub(-4) == ".lua" then
+            doReload = true
+        end
     end
-  end
-  
-  reloadWatcher = hs.pathwatcher.new(os.getenv('HOME') .. '/.hammerspoon/', reload):start()
+    if doReload then
+        hs.reload()
+    end
+end
+
+myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+hs.alert.show("Config reloaded", {
+    atScreenEdge = 1, -- 0: screen center (default); 1: top edge; 2: bottom edge . 
+})
