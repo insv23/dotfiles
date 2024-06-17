6b25665ce65d205c86fbd5ea163e5675f021d067
insv <insv23@outlook.com>
2024-06-17 19:12:27 +0800

    fix: KeepJamoAlive works well with other scripts

--- hammerspoon/autoPlayClickToKeepJamoAlive.lua
+++ hammerspoon/autoPlayClickToKeepJamoAlive.lua
@@ -1,31 +1,28 @@
 -- -- ??? : 与 QuickLaunch, hold cmd Q 冲突，只要 他们 启动，该脚本就不正常工作
+-- 参考: https://www.hammerspoon.org/go/#variablelife
+-- 如果 jamoSoundTimer 是 local 变量, 在其他快捷键启动后会被销毁
 
 -- -- 每隔 x 分钟播放指定的音频文件以保证 Jamo 音箱不关机
 -- -- Jamo 音箱大概 29 mins 不出声就关机
+
+-- 定义播放音频的时间间隔（以分钟为单位）
 local INTERVAL_MINS = 27
 local audioFile = "./audio/mouse-click.mp3"
-local interval_seconds = 60*INTERVAL_MINS
+local interval_seconds = 60 * INTERVAL_MINS
 
--- 导入Hammerspoon模块
-local timer = require "hs.timer"
-local notify = require "hs.notify"
-local sound = require "hs.sound"
-
--- 创建一个定时器，每隔 x 秒触发一次
-local soundTimer = timer.new(interval_seconds, function()
-    
+-- 播放音频文件的自定义函数
+local function playAudioFile()
     local outputDeviceName = hs.audiodevice.defaultOutputDevice():name()
-
-    -- 创建一个通知
-    -- notify.new({title=outputDeviceName, informativeText=""}):send()
     
     -- 检查当前音频输出设备是否为"外置耳机"
     if outputDeviceName == "外置耳机" then
         -- 播放指定的音频文件
-        sound.getByFile(audioFile):volume(0.05):play()
-        -- notify.new({title='Jamo', informativeText='is Click'}):send()
+        hs.sound.getByFile(audioFile):volume(0.005):play()
     end
-end)
+end
 
--- 启动定时器
-soundTimer:start()
+-- 创建一个定时器，每隔指定的秒数触发一次
+jamoSoundTimer = hs.timer.doEvery(interval_seconds, playAudioFile)
+
+-- 启动 Hammerspoon 时立即执行一次
+playAudioFile()
