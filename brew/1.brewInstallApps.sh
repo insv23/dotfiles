a2d0f155af54e54c7ada0c9a203008bc75a2fbfa
insv <insv23@outlook.com>
2024-07-24 19:10:25 +0800

    brew: Fix path

--- brew/1.brewInstallApps.sh
+++ brew/1.brewInstallApps.sh
@@ -2,14 +2,14 @@
 
 # ----- Brew Install Apps-----
 echo "brew 安装 mac 与 Linux 均使用的 apps..."
-xargs brew install < brew-both.txt
+xargs brew install < ~/.dotfiles/brew/brew-both.txt
 
 if uname -a | grep -q "Darwin"; then
     echo "brew 安装 mac apps..."
-    xargs brew install < brew-mac.txt
+    xargs brew install < ~/.dotfiles/brew/brew-mac.txt
 elif uname -a | grep -q "Linux"; then
     echo "brew 安装 linux apps..."
-    xargs brew install < brew-linux.txt
+    xargs brew install < ~/.dotfiles/brew/brew-linux.txt
 fi
 
 echo "brew 安装结束"
