e2bb5c6d10574d6503bd1910cfd17b9efc9154fa
insv <insv23@outlook.com>
2024-07-24 16:39:42 +0800

    tmux: Rm tmux plugins git submodules

---
+++ tmux/install_plugins.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 子模块太他妈折磨人了, 再也不用了
+# 在 .ignore 中添加 `tmux/plugins/` 将 tmux 插件全部不 track
+
+# 安装 tpm 并删除其 .git 文件
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+
+# 利用 tpm 的脚本安装插件
+~/.tmux/plugins/tpm/bin/install_plugins
