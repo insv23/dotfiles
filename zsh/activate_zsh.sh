b55ff63083ec303cfefc643de224c226a89eb704
insv <insv23@outlook.com>
2024-07-24 16:09:03 +0800

    zsh: Check zsh is installed before activated

--- zsh/activate_zsh.sh
+++ zsh/activate_zsh.sh
@@ -4,10 +4,20 @@
 current_shell=$(which "$SHELL")
 
 if echo "$current_shell" | grep -q "zsh"; then
-	echo "Current shell is already zsh: $current_shell"
+    zsh_version=$(zsh --version | cut -d' ' -f2)
+    echo "Current shell is already zsh: $current_shell (version $zsh_version)"
 else
-	echo "Current shell is not zsh. Attempting to switch to zsh..."
+    echo "Current shell is not zsh. Checking if zsh is available..."
 
-    sudo chsh -s "$(which zsh)" "$USER"
+    if which zsh >/dev/null 2>&1; then
+        echo "zsh($(which zsh) version $(zsh --version | cut -d' ' -f2)) is available. Attempting to switch to zsh..."
+        sudo chsh -s "$(which zsh)" "$USER"
+        echo "Shell changed. Please log out and log back in for changes to take effect."
+    else
+        echo "zsh is not installed or not in PATH. Please install zsh first."
+        echo "If Brew installed, run `brew install zsh && sudo chsh -s "$(brew --prefix)/bin/zsh" "$USER"`
+        echo "On Ubuntu, run `sudo apt update && apt-cache policy zsh && sudo apt install zsh -y && sudo chsh -s "$(which zsh)" "$USER"`
+    fi
 
     echo "Current shell path is: $current_shell"
+fi
