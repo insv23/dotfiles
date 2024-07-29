be8468b29743b77a6471b54ff947a2af3ce593e2
insv <insv23@outlook.com>
2024-07-29 21:47:26 +0800

    hammerspoon: Fix shorcuts conflicting

--- hammerspoon/QuickLaunch.lua
+++ hammerspoon/QuickLaunch.lua
@@ -1,25 +1,32 @@
 -- hs.hotkey.bind({'alt'}, '1', function () hs.application.launchOrFocus("Google Chrome") end)
 -- hs.hotkey.bind({'alt'}, '2', function () hs.application.launchOrFocus("Terminal") end)
---
+
 hs.hotkey.bind({ "alt" }, "a", function()
 	hs.application.launchOrFocus("Arc")
 end)
+
 hs.hotkey.bind({ "alt" }, "b", function()
 	hs.application.launchOrFocus("BotGem")
 end)
+
 hs.hotkey.bind({ "alt" }, "c", function()
 	hs.application.launchOrFocus("cursor")
 end)
+
 -- hs.hotkey.bind({'alt'}, 'e', function () hs.application.launchOrFocus("Finder") end)
 hs.hotkey.bind({ "alt" }, "n", function()
 	hs.application.launchOrFocus("Notion")
 end)
+
 hs.hotkey.bind({ "alt" }, "t", function()
 	hs.application.launchOrFocus("Telegram")
 end)
-hs.hotkey.bind({ "alt" }, "w", function()
-	hs.application.launchOrFocus("wechat")
-end)
+
+-- 与 shottr 的窗口截图冲突
+-- hs.hotkey.bind({ "alt" }, "w", function()
+-- 	hs.application.launchOrFocus("wechat")
+-- end)
+
 hs.hotkey.bind({ "alt" }, "k", function()
 	hs.application.launchOrFocus("kitty")
 end)
