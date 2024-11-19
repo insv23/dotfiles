7d9a7ad5ffd8dfe429ba941d51134e4259e592ac
insv <insv23@outlook.com>
2024-11-20 07:28:59 +0800

    zsh: change proxy port to 7890(Mihomo party) on Mac

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -14,7 +14,6 @@ alias lla="eza --color=always --long --git --icons=always -a"
 alias cp='cp -i'
 alias mv='mv -i'
 
-
 # git related aliases
 # in `../gitconfig`
 alias gag='git exec ag'
@@ -22,12 +21,10 @@ alias ginsv='git config user.name "insv";git config user.email "insv23@outlook.c
 # cd to git root directory
 alias cdgr='cd "$(git root)"'
 # create .gitignore template
-function gi() { curl -sLw "\n" https://www.toptal.com/developers/gitignore/api/$@ ;}
-
+function gi() { curl -sLw "\n" https://www.toptal.com/developers/gitignore/api/$@; }
 
 # zshrc
 alias szsh='source ~/.zshrc'
-
 
 # Homebrew
 alias brs='brew search'
@@ -36,18 +33,15 @@ alias bric='brew install --cask'
 alias bro='brew info'
 alias bru='brew uninstall'
 
-
 # nvim
 alias nv='nvim'
 
-
 # Update dotfiles
 dfu() {
-	(
-		cd ~/.dotfiles && git pull --ff-only && ./install -q
-	)
+  (
+    cd ~/.dotfiles && git pull --ff-only && ./install -q
+  )
 }
-
 
 # python
 alias py='python'
@@ -67,47 +61,47 @@ alias pyverm='pyenv virtualenv-delete'
 
 # Create a directory and cd into it
 mkcd() {
-	if [ ! -n "$1" ]; then
-		echo "Enter a directory name"
-	elif [ -d $1 ]; then
-		echo "'$1' already exists"
-	else
-		mkdir -pv $1 && cd $1
-	fi
+  if [ ! -n "$1" ]; then
+    echo "Enter a directory name"
+  elif [ -d $1 ]; then
+    echo "'$1' already exists"
+  else
+    mkdir -pv $1 && cd $1
+  fi
 }
 
 # proxy
 # Mac, WSL, Linux
 if uname -a | grep -q "Darwin"; then
-	# if (($(uname) == Darwin)); then // Not Work on Linux: https://blog.insv.xyz/shell-str-compare
-	export pxy_ip=127.0.0.1
-	export pxy_http_port=8888
-	export pxy_all_port=8889
+  # if (($(uname) == Darwin)); then // Not Work on Linux: https://blog.insv.xyz/shell-str-compare
+  export pxy_ip=127.0.0.1
+  export pxy_http_port=7890
+  export pxy_all_port=7890
 # elif (($(cat /proc/version | grep -c "WSL") == 1)); then
 elif cat /proc/version | grep -q "WSL"; then
-	export pxy_ip=$(cat /etc/resolv.conf | grep "nameserver" | cut -f 2 -d " ")
-	export pxy_http_port=7890
-	export pxy_all_port=7890
+  export pxy_ip=$(cat /etc/resolv.conf | grep "nameserver" | cut -f 2 -d " ")
+  export pxy_http_port=7890
+  export pxy_all_port=7890
 else
-	export pxy_http_port=20171
-	export pxy_all_port=20170
-	export pxy_http_port=20171
-	export pxy_all_port=20170
+  # Linux: v2ray
+  export pxy_http_port=20171
+  export pxy_all_port=20170
+  export pxy_http_port=20171
+  export pxy_all_port=20170
 fi
 pxyoff() {
-	unset http_proxy
-	unset https_proxy
-	unset all_proxy
-	echo -e "\033[31m[×] Proxy Off\033[0m"
+  unset http_proxy
+  unset https_proxy
+  unset all_proxy
+  echo -e "\033[31m[×] Proxy Off\033[0m"
 }
 pxyon() {
-	export no_proxy="localhost,127.0.0.1,localaddress,.localdomain.com"
-	export http_proxy="http://$pxy_ip:$pxy_http_port"
-	export https_proxy="http://$pxy_ip:$pxy_http_port"
-	export all_proxy="socks5://$pxy_ip:$pxy_all_port"
-	echo -e "\033[32m[√] Proxy On $pxy_ip:$pxy_http_port/$pxy_all_port\033[0m"
+  export no_proxy="localhost,127.0.0.1,localaddress,.localdomain.com"
+  export http_proxy="http://$pxy_ip:$pxy_http_port"
+  export https_proxy="http://$pxy_ip:$pxy_http_port"
+  export all_proxy="socks5://$pxy_ip:$pxy_all_port"
+  echo -e "\033[32m[√] Proxy On $pxy_ip:$pxy_http_port/$pxy_all_port\033[0m"
 }
-
 
 # docker
 alias dkd='docker compose down'
@@ -115,11 +109,10 @@ alias dku='docker compose up'
 alias dkud='docker compose up -d'
 alias caddy_reload='z caddy;docker compose exec -w /etc/caddy caddy caddy reload'
 function dkspp() {
-	id=$(docker ps -a | grep $1 | awk '{print $1}')
-	docker stop $id
-	docker container prune
+  id=$(docker ps -a | grep $1 | awk '{print $1}')
+  docker stop $id
+  docker container prune
 }
-
 
 # Tmux
 alias tmat='tmux at -t'
