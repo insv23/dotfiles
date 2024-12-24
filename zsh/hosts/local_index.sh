e7e909d921e19398f1e55ff64efb225636ee7e1a
insv <insv23@outlook.com>
2024-12-24 16:58:17 +0800

    zsh(local)+brew: 根据系统 hostname 自动创建对应的 local.zshrc 文件(如果不存的), 为 Linux brew 添加对应的模板 local.zshrc

--- zsh/hosts/local_index.sh
+++ zsh/hosts/local_index.sh
@@ -1,23 +1,75 @@
-# ----- 根据主机名选择相应的配置文件 -----
+#!/bin/sh
 
-# 获取当前主机名
-current_hostname=$(hostname)
+# 定义全局变量
+DOTFILES_ZSH_HOSTS="$HOME/.dotfiles/zsh/hosts"
 
-case "$current_hostname" in
-  "macmini")
-    source ~/.zsh/hosts/macmini.local.zshrc
-    ;;
-  "mba")
-    source ~/.zsh/hosts/mba.local.zshrc
-    ;;
-  "gpu")
-    source ~/.zsh/hosts/gpu.local.zshrc
-    ;;
+# 函数：创建主机配置文件
+create_host_config() {
+  hostname="$1"
+  target_file="$DOTFILES_ZSH_HOSTS/${hostname}.local.zshrc"
+
+  # 确保目标目录存在
+  mkdir -p "$DOTFILES_ZSH_HOSTS"
+
+  if [ ! -f "$target_file" ]; then
+    echo "File $target_file does not exist. Creating..."
+
+    uname_s=$(uname -s)
+    uname_m=$(uname -m)
+    if [ "$uname_s" = "Linux" ] && [ "$uname_m" = "x86_64" ]; then
+      example_file="$DOTFILES_ZSH_HOSTS/linux_amd64.local.zshrc.example"
+      if [ -f "$example_file" ]; then
+        cp "$example_file" "$target_file"
+        echo "Copied $example_file to $target_file"
+      else
+        echo "Example file $example_file not found. Creating an empty file."
+        touch "$target_file"
+      fi
+    else
+      touch "$target_file"
+      echo "Created empty file: $target_file"
+    fi
+  fi
+}
+
+# 函数：加载主机配置文件
+load_host_config() {
+  hostname="$1"
+  target_file="$DOTFILES_ZSH_HOSTS/${hostname}.local.zshrc"
+
+  if [ -f "$target_file" ]; then
+    . "$target_file"
+  else
+    echo "No specific configuration for this hostname. Please create a new one in $DOTFILES_ZSH_HOSTS/"
+  fi
+}
+
+# 函数：处理特殊主机名
+handle_special_hostname() {
+  hostname="$1"
+
+  case "$hostname" in
   "C20240108106238")
-    source ~/.zsh/hosts/lz-ycy.local.zshrc
+    . "$DOTFILES_ZSH_HOSTS/lz-ycy.local.zshrc"
+    return 0 # 成功加载，返回 0
     ;;
   *)
-    # 默认配置，如果没有匹配的主机名
-    echo "No specific configuration for this hostname. Please create a new one in ~/.dotfiles/zsh/hosts/"
+    return 1 # 不是特殊主机名，返回 1
     ;;
-esac
+  esac
+}
+
+main() {
+  current_hostname=$(hostname)
+
+  # 先处理特殊主机名(对应文件不为 hostname.local.zshrc 的)
+  # 返回值 0 表示成功/真(和其他高级语言相反)
+  handle_special_hostname "$current_hostname" && return
+
+  # 处理默认主机名(对应文件为 hostname.local.zshrc 的)
+  create_host_config "$current_hostname"
+  load_host_config "$current_hostname"
+}
+
+# 执行主逻辑
+main
