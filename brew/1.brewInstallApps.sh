1c88204378b9060e377e39855231eb3c13dc02d0
insv <insv23@outlook.com>
2024-12-24 17:22:58 +0800

    brew: 在安装 apps 前显式激活 brew 路径

--- brew/1.brewInstallApps.sh
+++ brew/1.brewInstallApps.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
+# 检查是否为 ARM Linux，如果是则退出
+if uname -a | grep -q "Linux" && uname -m | grep -q "aarch64"; then
+    echo "🚫 ARM Linux 暂不支持 Homebrew， 将退出 Homebrew 安装 apps..."
+    exit 0
+fi
+
+# 根据系统添加 Homebrew 路径
+if uname -a | grep -q "Darwin"; then
+    # macOS
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+else
+    # Linux
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+fi
+
 # ----- Brew Install Apps-----
 echo "🍺 brew 安装 mac 与 Linux 均使用的 apps..."
 xargs brew install < ~/.dotfiles/brew/brew-both.txt
