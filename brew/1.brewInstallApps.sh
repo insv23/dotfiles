ca0b12b5f6cbff66621e892e980df9de0305fc39
insv <insv23@outlook.com>
2024-12-24 17:40:07 +0800

    brew: 使用 Brewfile 来安装软件

--- brew/1.brewInstallApps.sh
+++ brew/1.brewInstallApps.sh
@@ -2,7 +2,7 @@
 
 # 检查是否为 ARM Linux，如果是则退出
 if uname -a | grep -q "Linux" && uname -m | grep -q "aarch64"; then
-    echo "🚫 ARM Linux 暂不支持 Homebrew， 将退出 Homebrew 安装 apps..."
+    echo "🚫 ARM Linux 暂不支持 Homebrew，退出安装 apps..."
     exit 0
 fi
 
@@ -10,25 +10,15 @@ fi
 if uname -a | grep -q "Darwin"; then
     # macOS
     eval "$(/opt/homebrew/bin/brew shellenv)"
-else
-    # Linux
-    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-fi
-
-# ----- Brew Install Apps-----
-echo "🍺 brew 安装 mac 与 Linux 均使用的 apps..."
-xargs brew install < ~/.dotfiles/brew/brew-both.txt
-echo
-
-if uname -a | grep -q "Darwin"; then
     echo "🍏 brew 安装 mac apps..."
-    xargs brew install < ~/.dotfiles/brew/brew-mac.txt
-    echo
-elif uname -a | grep -q "Linux"; then
+    brew bundle --file=~/.dotfiles/brew/Brewfile.mac
+else
+    # Linux (x86_64)
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
     echo "🐧 brew 安装 linux apps..."
-    xargs brew install < ~/.dotfiles/brew/brew-linux.txt
-    echo
+    brew bundle --file=~/.dotfiles/brew/Brewfile.linux
 fi
 
-echo "✅ brew 安装结束"
-echo
+# 安装通用应用
+echo "🍺 brew 安装通用 apps..."
+brew bundle --file=~/.dotfiles/brew/Brewfile.both
