a746f78bc587a0b4317809b7c37c578b6bd4c83a
insv <insv23@outlook.com>
2025-01-18 12:42:04 +0800

    nvim 使用系统剪贴板(实现远程复制到本地剪贴板)

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,3 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.winbar = "%=%m %f"
+
+-- 使用系统剪贴板
+-- 远程服务器中 tmux copy 后已经能自动发送到本地剪贴板中
+-- 让 nvim 复制内容自动落入系统剪贴板中，这样就能自动发送到本地剪贴板
+vim.opt.clipboard = "unnamedplus"
