b3f5e24c40c6bb91e20b266ff97d5227ffd498e2
insv <insv23@outlook.com>
2025-03-22 15:30:52 +0800

    nvim: 远程服务器复制后同步到本地需要有 xclip

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -24,11 +24,13 @@ vim.opt.clipboard = "unnamedplus"   -- 默认使用 + 寄存器（通常是 Ctrl
 -- ④ 少折腾命令行内的内容，完全是在不停踩坑。更烦人的是昨天能跑，今天就跑不了。
 --
 -- 问题② 解决记录
--- 2025-02-27
+-- 🫣2025-02-27
 -- Claude 3.7 一发入魂
 -- 提示我需要在 tmux.conf 中配置下面两列:
 -- set -g allow-passthrough on
 -- set -s set-clipboard on
+-- 🔥2025-03-22
+-- 远程 Linux 机器上需要有 xclip
 
 -- 禁用 LazyVim 的保存时自动格式化功能
 -- 虽然好像使用的 conform 进行格式化，但也只是 `空格cf` 这个生效
