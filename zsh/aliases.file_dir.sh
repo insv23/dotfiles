567fb31b576441db353f342189777b77b20eee93
insv <insv23@outlook.com>
2025-06-02 20:37:37 +0800

    alias: 移除自定义的 tree 命令, 使用成品

--- zsh/aliases.file_dir.sh
+++ zsh/aliases.file_dir.sh
@@ -30,24 +30,7 @@ alias lls='eza \
   --total-size \
   --git'
 
-# 利用 eza 定义一个 tree 命令
-# 不带任何数字, `tree` 默认展开 2 层
-# 可以自己加数字表示展开层级, `tree 1/3/...`
-tree() {
-  depth=2
-  if [ $# -gt 0 ]; then
-    case "$1" in
-    *[!0-9]*)
-      echo "Invalid argument: '$1'. Please provide a numeric depth value." >&2
-      return 1
-      ;;
-    *)
-      depth="$1"
-      ;;
-    esac
-  fi
-  lss -T -L="$depth"
-}
+
 
 # Create a directory and cd into it
 mkcd() {
