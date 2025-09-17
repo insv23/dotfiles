6bf0118633e065ac9cfa16da1aa37dca05de1a62
insv <insv23@outlook.com>
2025-09-17 17:35:48 +0800

    激活 Paste

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -7,6 +7,7 @@ source ~/.dotfiles/zsh/aliases/ffmpeg.sh
 source ~/.dotfiles/zsh/aliases/code-remote.sh
 source ~/.dotfiles/zsh/aliases/run_log.sh
 source ~/.dotfiles/zsh/aliases/tts.sh
+source ~/.dotfiles/zsh/aliases/paste.sh
 
 # PATH 分行显示
 alias echopath='echo $PATH | tr ":" "\n"'
