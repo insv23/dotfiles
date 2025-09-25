e0452fb0a18e69a3335b6bbb03494350b6b1fc5a
insv <insv23@outlook.com>
2025-09-25 13:16:35 +0800

    feat(hammerspoon): add app block list for cmd+q
    Added a block list functionality to prevent Cmd+Q from quitting specific applications.
    Apps can be blocked by their bundle ID, with Screen Sharing blocked by default.
    When attempting to use Cmd+Q on a blocked app, an alert is shown instead.

--- hammerspoon/HoldCmdQ.lua
+++ hammerspoon/HoldCmdQ.lua
@@ -6,6 +6,19 @@ cmdQDelay = 0.5
 cmdQTimer = nil
 cmdQAlert = nil
 
+-- Block list by bundle ID: apps here will have Cmd+Q disabled
+-- Default: Screen Sharing
+cmdQBlockBundleIDs = {
+  ["com.apple.ScreenSharing"] = true,
+}
+
+local function isCmdQBlocked(app)
+  if not app then return false end
+  local bid = app:bundleID()
+  if not bid then return false end
+  return cmdQBlockBundleIDs[bid] == true
+end
+
 function cmdQCleanup()
   hs.alert.closeSpecific(cmdQAlert)
   cmdQTimer = nil
@@ -22,7 +35,11 @@ end
 
 function startCmdQ()
   local app = hs.application.frontmostApplication()
-  cmdQTimer = hs.timer.doAfter(cmdQDelay, function() app:kill(); cmdQCleanup() end)
+  if isCmdQBlocked(app) then
+    hs.alert("已屏蔽 Cmd+Q：" .. app:name(), 0.8)
+    return
+  end
+  cmdQTimer = hs.timer.doAfter(cmdQDelay, function() app:terminate(); cmdQCleanup() end)
   cmdQAlert = hs.alert("hold to quit " .. app:name(), true)
 end
 
